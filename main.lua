3b51e0f234ee5e51e9cab4d7b56a2e95fbf420b5
Duc Mace <mcbubb4@gmail.com>
2025-04-02 21:41:56 +1100

    Quit Button

--- main.lua
+++ main.lua
@@ -81,6 +81,12 @@ function love.mousepressed(x, y, button, istouch, presses)
 
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function getDistance (x1, y1, x2, y2)
 	return math.sqrt((x2-x1)^2 + (y2-y1)^2)
 end
