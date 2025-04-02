9e3681be5318698bc555e85ea06b5219c10369d5
Duc Mace <mcbubb4@gmail.com>
2025-04-02 19:57:06 +1100

    Added scoring indicator

--- main.lua
+++ main.lua
@@ -2,12 +2,17 @@ function love.load()
 	love.window.setMode(1600,1200)
 
 	target = {}
-	target.x = love.math.random(0, 1600)
-	target.y = 0
+	target.x = love.math.random(200, 1400)
+	target.y = love.math.random(500, 1200)
 	target.radius = 40
 
 	timer = 0
 	score = 0
+	scoredx = 0
+	scoredx_x = 0
+	scoredx_y = 0
+	scoredx_timer = 60
+
 	distance = 0
 
 	font = love.graphics.newFont(30)
@@ -18,32 +23,52 @@ function love.update(dt)
 	timer = timer+1
 
 	if timer%60 == 0 then
-		target.y = love.math.random(0, 1200)
-		target.x = love.math.random(0, 1600)
+		target.x = love.math.random(200, 1400)
+		target.y = love.math.random(500, 1200)
 	end
 
+	if scoredx_timer < 180 then
+		scoredx_timer = scoredx_timer + 1
+	end
+
+	scoredx_y = scoredx_y - 1
+	
 end
 
 function love.draw()
-	--love.graphics.setColor(100/255,0,0)
+	love.graphics.setColor(1,1,1)
 
 	local scoreText = "Score: " .. score
 
 	love.graphics.setFont(font)
 	love.graphics.circle("fill", target.x, target.y, target.radius)
-	love.graphics.print(scoreText,200,500)
-	--love.graphics.print(distance,200,300)
-	--love.graphics.print(timer,200,700)
+	love.graphics.print(scoreText,200,200)
+	if scoredx_timer < 60 then
+		if scoredx == 1 then
+			love.graphics.setColor(0,1,0)
+			love.graphics.print("+1",scoredx_x,scoredx_y)
+			
+		elseif scoredx == -1 then
+			love.graphics.setColor(1,0,0)
+			love.graphics.print("-1",scoredx_x,scoredx_y)
+		end
+	end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
 	
 	distance = getDistance(x,y, target.x, target.y)
+	scoredx_x = x
+	scoredx_y = y
 
 	if distance <= target.radius then 
 		score = score + 1
+		scoredx = 1
+		scoredx_timer = 0
 	else 
 		score = score - 1
+		scoredx = -1
+		scoredx_timer = 0
 	end
 
 end
